058f132dcc5ce62050bf1a67c0986f11a71d05d2
ingmin <ymkwon3@gmail.com>
2022-04-05 23:38:44 +0900

    ~댓글중

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -8,10 +8,25 @@ import { history } from "../redux/configureStore";
 import { Button, Grid, Input } from "../elements";
 import CommentList from "../components/CommentList";
 
+import { useSelector, useDispatch } from "react-redux";
+import { actionCreators as postActions } from "../redux/modules/post";
+
 const PostDetail = props => {
+  const dispatch = useDispatch();
+  const pid = props.match.params.pid;
+
+  const post = useSelector(state => state.post.list).find((v) => v.id === pid);
+  
+  
+  React.useEffect(async() => {
+      if(post) return;
+      
+
+  }, []);
+  
   return (
     <>
-      <Post />
+      {post ? <Post {...post}/> : null}
       <Grid padding="16px" is_flex>
         <Input width="300px" ph="댓글 내용을 입력해주세요"></Input>
         <Button width="50px" margin="0 2px 0 2px">작성</Button>
